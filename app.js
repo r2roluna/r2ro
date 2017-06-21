f7393d9fb830684f8a567712e4ffcea83487f9e3
arturoluna <art@arturoluna.com>
2017-06-21 12:11:54 -0700

    updates to express

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 var express =  require('express');
+var reload = require('reload');
 var app = express();
 var dataFile = require('./data/data.json');
 
-app.set('port', process.env.PORT || 3000 );
+app.set('port', process.env.PORT || 8000 );
 app.set('appData', dataFile);
 
 app.use(express.static('public'));
 app.use(require('./routes/index'));
 app.use(require('./routes/speakers'));
 
-var server = app.listen(3000, function(){
-    console.log('Listening on Port' + app.get('port'));
+var server = app.listen(app.get('port'), function() {
+  console.log('Listening on port ' + app.get('port'));
 });
+
+console.log('app.js loaded');
+reload(server, app);
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
   res.send(`
-      <link rel="stylesheet" type="text/css" href="css/style.css">
-      <h1>Welcome to Roux Meetups</h1>
-      <img src="/images/misc/background.jpg" alt="background" style="height: 300px;">
-      <p>Roux Academy Meetups put together artists from all walks of life</p>
-      <script src="/reload/reload.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <h1>Arturo</h1>
+    <img src="/images/misc/background.jpg" alt="background" style="height: 300px;">
+    <p>Roux Academy Meetups put together artists from all walks of life</p>
+    <script src="/reload/reload.js"></script>
   `);
 });
 

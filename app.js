a8830b1dabd13fa3357abb98b87505cdf6af9230
arturoluna <art@arturoluna.com>
2017-06-24 18:31:29 -0700

    update .gitignore

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
-var express =  require('express');
-var reload = require('reload');
-var app = express();
-var dataFile = require('./data/data.json');
+const express = require('express')
+const app = express()
 
-app.set('port', process.env.PORT || 3000 );
-app.set('appData', dataFile);
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
-app.locals.siteTitle = 'Arturo Luna';
-
-app.use(express.static('public'));
-app.use(require('./routes/index'));
-app.use(require('./routes/speakers'));
-
-var server = app.listen(app.get('port'), function() {
-  console.log('Listening on port ' + app.get('port'));
-});
-
-console.log('app.js loaded');
-reload(server, app);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
